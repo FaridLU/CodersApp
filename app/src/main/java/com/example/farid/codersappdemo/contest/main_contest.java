ddfb0823cccbafffd59aff4462c7baeaaf302b19
faridbd <faridstudylu@gmail.com>
2018-05-23 18:07:32 +0600

    Some improvements on ContestList's Java and XML file

--- app/src/main/java/com/example/farid/codersappdemo/contest/main_contest.java
+++ app/src/main/java/com/example/farid/codersappdemo/contest/main_contest.java
@@ -73,41 +73,28 @@ public class main_contest extends AppCompatActivity {
     CoordinatorLayout main_contest;
     RelativeLayout activity_loading;
 
-
     public LinearLayout faceook, whatsapp, telegram, email, googleplus, messenger, bottom_she, share_bottom_sheet;
     public BottomSheetBehavior bottomSheetBehavior;
     RecyclerView recyclerView;
     private Context mContext = this;
     private AnimatedCircleLoadingView animatedCircleLoadingView;
+    CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_contest);
 
-        faceook = findViewById(R.id.facebook_share);
-        whatsapp = findViewById(R.id.whatsapp_share);
-        telegram = findViewById(R.id.telegram_share);
-        googleplus = findViewById(R.id.googleplus_share);
-        messenger = findViewById(R.id.messenger_share);
-        email = findViewById(R.id.email_share);
-        share_bottom_sheet = findViewById(R.id.share_bottom_sheet);
-        bottomSheetBehavior = BottomSheetBehavior.from(share_bottom_sheet);
+        setTitle("Contest's List");
 
+        FindVIewByID();
+
+        bottomSheetBehavior = BottomSheetBehavior.from(share_bottom_sheet);
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
-        recyclerView = findViewById(R.id.recyclerview);
 
         All_Contest_list = (List<ContestActivity>) getIntent().getSerializableExtra("list");
 
-        System.out.println(All_Contest_list.size() + " ******************* 2");
-
-        RecyclerAdapter myAdapter = new RecyclerAdapter(mContext, All_Contest_list, faceook, whatsapp, telegram, googleplus, email, messenger, bottomSheetBehavior, share_bottom_sheet);
-        recyclerView.setLayoutManager(new VegaLayoutManager());
-        recyclerView.setAdapter(myAdapter);
-        //recyclerView.setNestedScrollingEnabled(false);
-        recyclerView.smoothScrollToPosition(0);
-
-        CoordinatorLayout coordinatorLayout = findViewById(R.id.contest_layout);
+        SetAdapter();
 
         final Snackbar snackbar = Snackbar.make(coordinatorLayout, "List may corrupted for more accurate result reload the page", Snackbar.LENGTH_INDEFINITE);
         snackbar.setAction("OKAY", new View.OnClickListener() {
@@ -118,9 +105,312 @@ public class main_contest extends AppCompatActivity {
         });
         snackbar.show();
     }
+    public void SetAdapter(){
+        RecyclerAdapter myAdapter = new RecyclerAdapter(mContext, All_Contest_list, faceook, whatsapp, telegram, googleplus, email, messenger, bottomSheetBehavior, share_bottom_sheet);
+        recyclerView.setLayoutManager(new VegaLayoutManager());
+        recyclerView.setAdapter(myAdapter);
+        //recyclerView.setNestedScrollingEnabled(false);
+        recyclerView.smoothScrollToPosition(0);
+    }
+    public void FindVIewByID(){
+        faceook = findViewById(R.id.facebook_share);
+        whatsapp = findViewById(R.id.whatsapp_share);
+        telegram = findViewById(R.id.telegram_share);
+        googleplus = findViewById(R.id.googleplus_share);
+        messenger = findViewById(R.id.messenger_share);
+        email = findViewById(R.id.email_share);
+        share_bottom_sheet = findViewById(R.id.share_bottom_sheet);
+        recyclerView = findViewById(R.id.recyclerview);
+        coordinatorLayout = findViewById(R.id.contest_layout);
+    }
 
     @Override
     public void onBackPressed() {
         startActivity(new Intent(main_contest.this, MainActivity.class));
+    }
+    public class BackgroundRefreshTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Do Something before showing result
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            // After complete sync
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            // Do somthing while collecting data
+            make_list_for_Codeforces();
+            make_list_for_CodeChef();
+            Arrange_Contest_List();
+            return null;
+        }
+    }
+
+    public void Arrange_Contest_List() {
+        Collections.sort(All_Contest_list, new Comparator<ContestActivity>() {
+            @Override
+            public int compare(ContestActivity x, ContestActivity y) {
+
+                int Start_Day_X = Integer.valueOf(x.start_date.substring(0, 2));
+                int Start_Day_Y = Integer.valueOf(y.start_date.substring(0, 2));
+
+                int Start_Month_X = Month(x.start_information.substring(0, 3));
+                int Start_Month_Y = Month(y.start_information.substring(0, 3));
+
+                int Start_Year_X = Integer.valueOf(x.start_information.substring(4, 8));
+                int Start_Year_Y = Integer.valueOf(y.start_information.substring(4, 8));
+
+                int Start_Hour_X = Integer.valueOf(x.start_information.substring(9, 11));
+                int Start_Hour_Y = Integer.valueOf(y.start_information.substring(9, 11));
+
+                int Start_Min_X = Integer.valueOf(x.start_information.substring(12, 14));
+                int Start_Min_Y = Integer.valueOf(y.start_information.substring(12, 14));
+
+
+                if (x.contest_type == y.contest_type) {
+                    if (Start_Year_X == Start_Year_Y) {
+                        if (Start_Month_X == Start_Month_Y) {
+                            if (Start_Day_X == Start_Day_Y) {
+                                if (Start_Hour_X == Start_Hour_Y) {
+                                    return Start_Min_X < Start_Min_Y ? -1 : 1;
+                                }
+                                return Start_Hour_X < Start_Hour_Y ? -1 : 1;
+                            }
+                            return Start_Day_X < Start_Day_Y ? -1 : 1;
+                        }
+                        return Start_Month_X < Start_Month_Y ? -1 : 1;
+                    }
+                    return Start_Year_X < Start_Year_Y ? -1 : 1;
+                }
+                return x.contest_type < y.contest_type ? -1 : 1;
+            }
+        });
+    }
+
+    public void make_list_for_Codeforces() {
+        //System.out.println("Codefoces -> ");
+        /*new Thread(new Runnable() {
+            @Override
+            public void run() {*/
+        try {
+            String url = "http://codeforces.com/contests?complete=true";
+            Document doc = Jsoup.connect(url).get();
+            Elements el = doc.getElementsByClass("datatable").get(0).select("tbody").select("tr");
+
+            int len = el.size();
+
+            for (int i = 1; i < len; i++) {
+
+                String contest_name = el.get(i).select("td").get(0).text(); // Contest_Name
+                String contest_date = el.get(i).select("td").get(2).text();
+                String contest_duration = el.get(i).select("td").get(3).text();
+                String contest_link = null;
+
+                int type = 2;
+
+                if(contest_name.endsWith("Enter »")) {
+                    type = 1;
+                    int ln = contest_name.length();
+                    contest_name = contest_name.substring(0, ln-7);
+                    contest_link = el.get(i).select("td").get(0).select("a").attr("abs:href");
+                }
+
+                String start_date = contest_date.substring(4, 6);
+                String start_information = contest_date.replaceFirst("/" + start_date + "/", " ");
+
+                String end_date_information = GiveMeEnd(contest_date, contest_duration);
+
+                if (end_date_information.length() == 0) continue;
+
+                String end_date = end_date_information.substring(8, 10);
+                int le = end_date_information.length();
+
+                String end_information = end_date_information.substring(4, 7) + " " + end_date_information.substring(le - 4, le) + " " + end_date_information.substring(11, 20);
+                end_information = end_information.substring(0, end_information.length()-4);
+
+                All_Contest_list.add(new ContestActivity(start_date, start_information, end_date, end_information, contest_name, contest_link, R.drawable.codeforces, type, "codeforces"));
+                //System.out.println("Codefoces -> 2 ");
+            }
+
+        } catch (Exception e) {
+
+        }
+
+            /*}
+        }).start();*/
+    }
+
+    public void make_list_for_CodeChef() {
+
+        /*new Thread(new Runnable() {
+            @Override
+            public void run() {*/
+        try {
+            String url = "https://www.codechef.com/contests";
+            String url2 = "https://www.codechef.com/";
+
+            Document doc = Jsoup.connect(url).get();
+            Elements el = doc.getElementsByClass("dataTable");
+
+            for(int k=0; k<2; k++) {
+                Elements table = el.get(k).select("tbody");
+                Elements rows = table.select("tr");
+
+                for(int i=0; i<rows.size(); i += 1) {
+
+                    Element row0 = rows.get(i);
+                    Elements cols0 = row0.select("td");
+
+                    String tmp;
+                    String ContestName = row0.select("td").select("a").text();
+
+                    tmp = row0.select("td").text();
+
+                    String[] arr = new String[10];
+                    int ind = -1;
+                    String without_ContestName = "";
+                    for(int j=0; j<tmp.length(); j++) {
+                        int cInd = 0, tInd = j;
+
+                        if(tmp.charAt(j) == ' ') {
+                            arr[++ind] = without_ContestName;
+                            without_ContestName = "";
+                            continue;
+                        }
+                        int len = ContestName.length();
+
+                        while(cInd < len && ContestName.charAt(cInd) == (tmp.charAt(tInd))) {
+                            cInd++; tInd++;
+                        }
+                        if(cInd == len) j = tInd;
+                        else {
+                            without_ContestName += tmp.charAt(j);
+                        }
+                    }
+                    arr[++ind] = without_ContestName;
+
+                    String ContestLink = url2 + "/" + arr[0];
+                    String StartDate = arr[2] + " " + arr[3];
+                    String StartTime = arr[4];
+                    String EndDate = arr[6] + " " + arr[7];
+                    String EndTime = arr[8];
+
+                    EndTime = EndTime.substring(0, 5);
+                    StartTime = StartTime.substring(0, 5);
+
+                    All_Contest_list.add(new ContestActivity(arr[1], StartDate + " " + StartTime, arr[5], EndDate + " " + EndTime , ContestName, ContestLink, R.drawable.codechef, k+1, "codechef"));
+                }
+            }
+        }catch (Exception e) {
+
+        }
+            /*}
+        }).start();*/
+    }
+
+    public static String GiveMeEnd(String start_date, String duration) {
+
+        Helpful_Codes(); // Nothing but useful
+
+        System.out.println(start_date + " " + duration);
+
+        int year = Integer.valueOf(start_date.substring(7, 11));
+        int month = Month(start_date.substring(0, 3));
+        int date = Integer.valueOf(start_date.substring(4, 6));
+        int hour = Integer.valueOf(start_date.substring(12, 14));
+        int min = Integer.valueOf(start_date.substring(15, 17));
+        int len = duration.length();
+        int duration_min = 0, duration_hour = 0, duration_day = 0;
+
+        if(len-2 >= 0) {
+            duration_min = Integer.valueOf(duration.substring(len-2, len));
+            len -= 2;
+        }
+        else if(len == 1) {
+            duration_min = Integer.valueOf(duration.substring(len-1, len));
+            len--;
+        }
+        len--;
+
+        if(len-2 >= 0) {
+            duration_hour = Integer.valueOf(duration.substring(len-2, len));
+            len -= 2;
+        }
+        else if(len == 1) {
+            duration_hour = Integer.valueOf(duration.substring(len-1, len));
+            len--;
+        }
+        len--;
+
+        if(len-2 >= 0) {
+            duration_day = Integer.valueOf(duration.substring(len-2, len));
+            len -= 2;
+        }
+        else if(len == 1) {
+            duration_day = Integer.valueOf(duration.substring(len-1, len));
+            len--;
+        }
+        len--;
+
+        DateFormat format = new SimpleDateFormat("MMM/dd/yyyy HH:mm", Locale.US);
+
+        String End_Time = null;
+        String ans = "kunta nai";
+
+        try {
+
+            Date parse_date = format.parse(start_date);
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(parse_date);
+
+            System.out.println(parse_date.toString());
+            calendar.add(Calendar.MINUTE, duration_min);
+            calendar.add(Calendar.HOUR, duration_hour);
+            calendar.add(Calendar.DAY_OF_MONTH, duration_day);
+
+            End_Time = calendar.getTime().toString();
+            ans = "aise";
+
+        }catch(Exception e) {
+            System.out.println("Something Went wrong");
+        }
+        //System.out.println(End_Time);
+
+        return End_Time;
+    }
+
+    public static int Month(String month) {
+        if(month.toLowerCase().equals("jan")) return 1;
+        if(month.toLowerCase().equals("feb")) return 2;
+        if(month.toLowerCase().equals("mar")) return 3;
+        if(month.toLowerCase().equals("apr")) return 4;
+        if(month.toLowerCase().equals("may")) return 5;
+        if(month.toLowerCase().equals("jun")) return 6;
+        if(month.toLowerCase().equals("jul")) return 7;
+        if(month.toLowerCase().equals("aug")) return 8;
+        if(month.toLowerCase().equals("sep")) return 9;
+        if(month.toLowerCase().equals("oct")) return 10;
+        if(month.toLowerCase().equals("nov")) return 11;
+        if(month.toLowerCase().equals("dec")) return 12;
+        return 0;
+    }
+
+    public static void Helpful_Codes(){
+        /**
+         Helpful Links:
+         --------------
+         http://www.digitstory.com/recyclerview-multiple-viewholders/
+         https://developer.android.com/reference/java/text/SimpleDateFormat.html
+         https://stackoverflow.com/questions/22917562/adding-n-number-of-days-in-a-given-particular-date
+         https://stackoverflow.com/questions/1459656/how-to-get-the-current-time-in-yyyy-mm-dd-hhmisec-millisecond-format-in-java?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+         https://stackoverflow.com/questions/18733582/calculating-a-future-date?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+         */
     }
 }
