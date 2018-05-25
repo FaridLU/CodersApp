ac6c360b77abe160f1079500e36fbb8cb250ac78
Farid ul Islam Chowdhury <faridstudylu@gmail.com>
2018-05-25 17:32:42 +0600

    Added Navigation Drawer Demo

--- app/src/main/java/com/example/farid/codersappdemo/MainActivity.java
+++ app/src/main/java/com/example/farid/codersappdemo/MainActivity.java
@@ -3,36 +3,148 @@ package com.example.farid.codersappdemo;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
-import android.support.v7.app.AlertDialog;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 
 import com.example.farid.codersappdemo.contest.main_contest;
 import com.example.farid.codersappdemo.friends.friend_list;
+import com.shrikanthravi.customnavigationdrawer2.data.MenuItem;
+import com.shrikanthravi.customnavigationdrawer2.widget.SNavigationDrawer;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import java.util.ArrayList;
+import java.util.List;
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
-    CardView contests_card, friends_card, ranking_card, calculator_card, settings_card;
+public class MainActivity extends AppCompatActivity {
 
+    SNavigationDrawer sNavigationDrawer;
+    Class fragmentClass;
+    public static Fragment fragment;
+    private int prev_position = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         getSupportActionBar().hide();
 
-        contests_card = findViewById(R.id.contests_card);
-        friends_card = findViewById(R.id.friends_card);
-        ranking_card = findViewById(R.id.ranking_card);
-        calculator_card = findViewById(R.id.calculator_card);
-        settings_card = findViewById(R.id.settings_card);
 
-        contests_card.setOnClickListener(this);
-        friends_card.setOnClickListener(this);
-        ranking_card.setOnClickListener(this);
-        calculator_card.setOnClickListener(this);
-        settings_card.setOnClickListener(this);
+
+        sNavigationDrawer = findViewById(R.id.navigationDrawer);
+
+        List<MenuItem> menuItems = new ArrayList<>();
+
+        menuItems.add(new MenuItem("DashBoard",R.drawable.material_background));
+        menuItems.add(new MenuItem("Contests",R.drawable.material_background));
+        menuItems.add(new MenuItem("Friends",R.drawable.material_background));
+        menuItems.add(new MenuItem("Ranking",R.drawable.material_background));
+        menuItems.add(new MenuItem("Calculator",R.drawable.material_background));
+        menuItems.add(new MenuItem("My Profile",R.drawable.material_background));
+        menuItems.add(new MenuItem("Exit",R.drawable.material_background));
+
+        sNavigationDrawer.setAppbarTitleTV("Dashboard");
+        sNavigationDrawer.setMenuItemList(menuItems);
+        fragmentClass =  dashboard_activity.class;
+
+        try {
+            Log.d("tagtag", "mainactivity");
+
+            fragment = (Fragment) fragmentClass.newInstance();
+            Log.d("tagtag", "initialization complete");
+        } catch (Exception e) {
+            System.out.println( "catch e hamaise");
+            e.printStackTrace();
+        }
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out).replace(R.id.frameLayout, fragment).commit();
+        }
+
+
+
+
+        sNavigationDrawer.setOnMenuItemClickListener(new SNavigationDrawer.OnMenuItemClickListener() {
+            @Override
+            public void onMenuItemClicked(int position) {
+                System.out.println("Position "+position);
+
+                switch (position){
+                    case 0:{
+                        prev_position= 0;
+                        fragmentClass = dashboard_activity.class;
+                        break;
+                    }
+                    case 1:{
+                        prev_position= 1;
+                        fragmentClass = loading_activity.class;
+                        break;
+                    }
+                    case 2:{
+                        prev_position= 2;
+                        System.out.println("friend paisi");
+                        fragmentClass = friend_list.class;
+                        break;
+                    }
+                    case 6:{
+                        onBackPressed();
+                        break;
+                    }
+
+                }
+                sNavigationDrawer.setDrawerListener(new SNavigationDrawer.DrawerListener() {
+
+                    @Override
+                    public void onDrawerOpened() {
+
+                    }
+
+                    @Override
+                    public void onDrawerOpening(){
+
+                    }
+
+                    @Override
+                    public void onDrawerClosing(){
+                        System.out.println("Drawer closed");
+
+                        try {
+                            fragment = (Fragment) fragmentClass.newInstance();
+                            System.out.println("close howar bade try");
+                        } catch (Exception e) {
+                            System.out.println("close howar bade catch");
+                            e.printStackTrace();
+                        }
+
+                        if (fragment != null) {
+                            FragmentManager fragmentManager = getSupportFragmentManager();
+                            fragmentManager.beginTransaction().setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out).replace(R.id.frameLayout, fragment).commit();
+
+                        }
+                    }
+
+                    @Override
+                    public void onDrawerClosed() {
+
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        System.out.println("State "+newState);
+                    }
+                });
+            }
+        });
 
     }
 
@@ -56,22 +168,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
+                if (fragment != null) {
+                    try {
+                        fragment = (Fragment) fragmentClass.newInstance();
+                    } catch (InstantiationException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                    if(prev_position == 0) sNavigationDrawer.setAppbarTitleTV("Dashboard");
+                    else if(prev_position == 1) sNavigationDrawer.setAppbarTitleTV("Contests");
+                    else if(prev_position == 2) sNavigationDrawer.setAppbarTitleTV("Friends");
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction().setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out).replace(R.id.frameLayout, fragment).commit();
+
+                }
             }
         });
         builder.show();
     }
 
-    @Override
-    public void onClick(View v) {
-        Intent intent;
 
-        switch (v.getId()) {
-            case R.id.contests_card: intent = new Intent(this, loading_activity.class); startActivity(intent); break;
-            case R.id.friends_card: intent = new Intent(this, friend_list.class); startActivity(intent);break;
-            case R.id.ranking_card: intent = new Intent(this, loading_activity.class);startActivity(intent);break;
-            case R.id.calculator_card: intent = new Intent(this, loading_activity.class);startActivity(intent);break;
-            case R.id.settings_card: intent = new Intent(this, loading_activity.class);startActivity(intent);break;
-        }
-
-    }
 }
